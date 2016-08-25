aa96261b7cad86e88becb12365a4d50c7fadf865
Saswat <saswat@Saswats-MacBook-Pro.local>
2016-08-25 17:55:58 +0530

    assets added

---
+++ Assets/csvReaderScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class wordList{
+	public List<string> words;
+	wordList()	{
+		words = new List<string> ();
+	}
+}
+
+[System.Serializable]
+public class optionsData{
+	public string text;
+	public int nextIndex;
+	public float economyChange;
+}
+
+[System.Serializable]
+public class stateData{
+	public string text;
+	public List<optionsData> options;
+	public stateData(){
+		options = new List<optionsData> ();
+	}
+}
+
+public class csvReaderScript : MonoBehaviour {
+
+	public TextAsset csvFile;
+	string[,] grid;
+	int i, j;
+	[SerializeField] 
+	public wordList[] list;
+	public List<stateData> stateDataList;
+	stateData currentData;
+
+	void Start () {
+		list = new wordList[9];
+		stateDataList = new List<stateData> ();
+		currentData = new stateData ();
+	}
+
+	public void readCSV(){
+		grid = CSVReader.SplitCsvGrid(csvFile.text);
+		int x = grid.GetUpperBound (0);
+		int y = grid.GetUpperBound (1);
+
+		for (i = 0; i < x; i++) {
+			for (j = 0; j < y; j++) {
+				if (grid [i, j] != null && grid [i, j].Length != 0) {
+					grid [i, j] = grid [i, j].ToUpper();
+					list [grid [i, j].Length - 1].words.Add (grid [i, j]);
+				}
+			}
+		}
+
+		for (i = 0; i < list.Length; i++) {
+			list [i].words.Sort ();
+		}
+	}
+
+	public void readCamperCSV(){
+		grid = CSVReader.SplitCsvGrid(csvFile.text);
+		int x = grid.GetUpperBound (0);
+		int y = grid.GetUpperBound (1);
+		int size = 0;
+		optionsData od = new optionsData();
+		for (i = 0; i < x; i++) {
+			if (grid [i, 0] != null && grid [i, 0].Length != 0) {
+				currentData.text = grid [i, 0];
+				size =  Int32.Parse(grid[i,1]);
+				for (j = 0; j < size; j++) {
+					od.text = grid [i, j + 2];
+					od.nextIndex = Int32.Parse(grid [i, j + 2 + size]);
+					od.economyChange = Int32.Parse(grid [i, j + 2 + size * 2]);
+					currentData.options.Add (od);
+					od = new optionsData ();
+				}
+				stateDataList.Add (currentData);
+				currentData = new stateData ();
+
+				//currentData.text = grid [i, 0];
+			}
+
+		}
+		
+	}
+}
---
+++ Assets/jsonWriterScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+using UnityEditor;
+using System.IO;
+
+public class jsonWriterScript : MonoBehaviour {
+
+	public csvReaderScript csv;
+	string jsonString;
+	int i,j;
+	void Start () {
+		csv = GetComponent<csvReaderScript> ();
+		StartCoroutine ("delay");
+	}
+
+	IEnumerator delay()	{
+		yield return new WaitForSeconds (0.5f);
+		csv.readCamperCSV ();
+	//	csv.readCSV ();
+	//	createJSON ();
+	}
+
+	void createJSON(){
+		JSONNode mainNode = new JSONClass();
+		JSONArray mainArray = new JSONArray ();
+		JSONArray array = new JSONArray ();
+
+		for (i = 0; i < csv.list.Length; i++) {
+			for (j = 0; j < csv.list [i].words.Count; j++) {
+				array.Add (csv.list [i].words [j]);
+			}
+			mainArray.Add (array);
+			array = new JSONArray ();
+		}
+		mainNode.Add ("Root", mainArray);
+		//print (mainNode.ToString ());
+		writeJSON(mainNode.ToString());
+	}
+
+	void writeJSON(string data){
+		string path = "Assets/BOLLYWOOD.json";
+		using (FileStream fs = new FileStream (path, FileMode.Create)) {
+			using (StreamWriter writer = new StreamWriter (fs)) {
+				writer.Write (data);
+			}
+		}
+		UnityEditor.AssetDatabase.Refresh ();
+	}
+}
